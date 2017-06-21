c3e5afd047e9b73ac9f405e1f9927d83e779369b
Rai <jraicr@gmail.com>
2017-06-21 21:26:51 +0200

    Add preliminar factories setup for lobbies.

---
+++ Assets/Master Server/Scripts/FactoriesSetup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Barebones.MasterServer;
+using UnityEngine;
+
+namespace Thieves.GameServer.Lobby {
+    public class FactoriesSetup : MonoBehaviour {
+        public HelpBox Header = new HelpBox() {
+            Text = "This script adds lobby factories to the module " +
+                   "(1vs1, 2vs2, 4vs4)"
+        };
+
+        void Start() {
+            // Add lobby factories
+
+            var module = FindObjectOfType<LobbiesModule>();
+
+            module.AddFactory(new LobbyFactoryAnonymous("1 vs 1",
+                module, DemoLobbyFactories.OneVsOne));
+
+            module.AddFactory(new LobbyFactoryAnonymous("2 vs 2",
+                module, DemoLobbyFactories.TwoVsTwoVsFour));
+
+            module.AddFactory(new LobbyFactoryAnonymous("4 vs 4",
+                module, DemoLobbyFactories.ThreeVsThreeQueue));
+        }
+    }
+}
